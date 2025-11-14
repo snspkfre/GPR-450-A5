abc0f7c8d19106348d804b9d6fa42b78593529cc
olivia.santamauro <olivia.santamauro@mymail.champlain.edu>
2025-11-13 23:11:44 -0500

    added more functions to unit test

--- cat/build_vs/cat/cat/cat_unit_test.h
+++ cat/build_vs/cat/cat/cat_unit_test.h
@@ -7,49 +7,52 @@
 cat_interface_begin;
 
 typedef float vec3f[3];
-typedef float (*testf_vec3f_vec3f)(const vec3f, const vec3f);
-typedef float* (*testfp_vec3f_vec3f_vec3f)(vec3f, const vec3f, const vec3f);
+typedef float (*test_f_vec3f_vec3f)(const vec3f, const vec3f);
+typedef float* (*test_fp_vec3f_vec3f_vec3f)(vec3f, const vec3f, const vec3f);
 
 float dotProduct(const vec3f v1, const vec3f v2);
 float* crossProduct(vec3f v_out, const vec3f v1, const vec3f v2);
-//float* vec3fProj(vec3f v_out, const vec3f v1, const vec3f v2);
+float* vec3fProj(vec3f v_out, const vec3f v1, const vec3f v2);
+float* vec3fAdd(vec3f v_out, const vec3f v1, const vec3f v2);
+//scalar vector / component multiplication
 
-void initTest_f_v3f_v3f(testf_vec3f_vec3f* funcPtr)
+
+void initTest_f_v3f_v3f(test_f_vec3f_vec3f* funcPtr)
 {
     unused(funcPtr);
 }
 
-void initTest_fp_v3f_v3f_v3f(testfp_vec3f_vec3f_vec3f* funcPtr)
+void initTest_fp_v3f_v3f_v3f(test_fp_vec3f_vec3f_vec3f* funcPtr)
 {
     unused(funcPtr);
 }
 
-float execTest_f_v3f_v3f(testf_vec3f_vec3f, const vec3f, const vec3f);
-float* execTest_fp_v3f_v3f_v3f(testfp_vec3f_vec3f_vec3f, vec3f, const vec3f, const vec3f);
+float execTest_f_v3f_v3f(test_f_vec3f_vec3f, const vec3f, const vec3f);
+float* execTest_fp_v3f_v3f_v3f(test_fp_vec3f_vec3f_vec3f, vec3f, const vec3f, const vec3f);
 
-void cleanTest_f_v3f_v3f(testf_vec3f_vec3f* funcPtr)
+void cleanTest_f_v3f_v3f(test_f_vec3f_vec3f* funcPtr)
 {
     unused(funcPtr);
 }
 
-void cleanTest_fp_v3f_v3f_v3f(testfp_vec3f_vec3f_vec3f* funcPtr)
+void cleanTest_fp_v3f_v3f_v3f(test_fp_vec3f_vec3f_vec3f* funcPtr)
 {
     unused(funcPtr);
 }
 
 #define InitTest(X) _Generic((X),						    \
-                testf_vec3f_vec3f*: initTest_f_v3f_v3f,		\
-         testfp_vec3f_vec3f_vec3f*: initTest_fp_v3f_v3f_v3f  \
+                test_f_vec3f_vec3f*: initTest_f_v3f_v3f,		\
+         test_fp_vec3f_vec3f_vec3f*: initTest_fp_v3f_v3f_v3f  \
               )(X)
 
 #define ExecTest(X, ...) _Generic((X),						\
-                testf_vec3f_vec3f: execTest_f_v3f_v3f,		\
-         testfp_vec3f_vec3f_vec3f: execTest_fp_v3f_v3f_v3f  \
+                test_f_vec3f_vec3f: execTest_f_v3f_v3f,		\
+         test_fp_vec3f_vec3f_vec3f: execTest_fp_v3f_v3f_v3f  \
               )(X, __VA_ARGS__)
 
 #define CleanTest(X) _Generic((X),						    \
-                testf_vec3f_vec3f*: cleanTest_f_v3f_v3f,		\
-         testfp_vec3f_vec3f_vec3f*: cleanTest_fp_v3f_v3f_v3f \
+                test_f_vec3f_vec3f*: cleanTest_f_v3f_v3f,		\
+         test_fp_vec3f_vec3f_vec3f*: cleanTest_fp_v3f_v3f_v3f \
               )(X)
 
 void runUnitTests(void);
